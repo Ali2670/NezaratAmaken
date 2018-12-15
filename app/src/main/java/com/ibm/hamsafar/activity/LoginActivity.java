ca70bb2246031fb1a88b82aae880ca0bf767ac21
rahimi <m.rahimi@behpardaz.net>
2018-12-15 15:45:48 +0330

    save login info ( mobile number and user id ) in shared preferences

--- app/src/main/java/com/ibm/hamsafar/activity/LoginActivity.java
+++ app/src/main/java/com/ibm/hamsafar/activity/LoginActivity.java
@@ -4,10 +4,12 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.preference.PreferenceManager;
 import android.support.design.widget.TextInputLayout;
 import android.view.View;
 import android.widget.Button;
@@ -43,6 +45,7 @@ public class LoginActivity extends Activity {
     private ProgressBar progressBarCircle = null;
     private RelativeLayout timerLayout = null;
     private LinearLayout blurLayout = null;
+    private SharedPreferences sharedPreferences;
 
     //check login button to send activation code or check activation code
     private boolean loggedIn = false;
@@ -57,6 +60,8 @@ public class LoginActivity extends Activity {
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.login);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         mobileLayout = findViewById(R.id.login_mobile_layout);
         mobile = findViewById(R.id.login_mobile);
@@ -131,6 +136,7 @@ public class LoginActivity extends Activity {
         int default_code = 1234;
         if (act_code == default_code) {
             codeLayout.setError(null);
+            saveLoginInfo();
             launchEnrolScreen();
         } else {
             codeLayout.setError(getResources().getString(R.string.Exc_600003));
@@ -161,6 +167,18 @@ public class LoginActivity extends Activity {
 
         }.start();
         countDownTimer.start();
+    }
+
+    private void saveLoginInfo() {
+        /*
+        *
+        * get user id from DB
+        *
+        * */
+
+        //savePreferences("user_id", id );
+        savePreferences("mobile_number", mobile.getText().toString().trim() );
+
     }
 
     /**
@@ -217,6 +235,13 @@ public class LoginActivity extends Activity {
             return false;
         }
         return true;
+    }
+
+    private void savePreferences(String key, String value) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key, value);
+        editor.commit();
     }
 
 }
