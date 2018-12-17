e435caa726aa7915a1e148181f300e283d1a00af
rahimi <m.rahimi@behpardaz.net>
2018-12-17 15:58:33 +0330

    interface for handling long click on recyclerView items

---
+++ app/src/main/java/com/ibm/hamsafar/adapter/ItemLongClickListener.java
@@ -0,0 +1,12 @@
+package com.ibm.hamsafar.adapter;
+
+import android.view.View;
+
+/**
+ * Created by maryam on 12/17/2018.
+ */
+public interface ItemLongClickListener {
+
+    void onItemLongClick(View v, int pos);
+
+}
