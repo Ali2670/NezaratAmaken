4d80f45ed9ce2f749123f82fdf2e4d812231650c
rahimi <m.rahimi@behpardaz.net>
2018-12-17 12:36:10 +0330

    keep trip info

---
+++ app/src/main/java/com/ibm/hamsafar/object/TripInfo.java
@@ -0,0 +1,53 @@
+package com.ibm.hamsafar.object;
+
+/**
+ * Created by maryam on 12/17/2018.
+ */
+public class TripInfo {
+
+    private String port;
+    private String des;
+    private String start;
+    private String end;
+    private String trans;
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getTrans() {
+        return trans;
+    }
+
+    public void setTrans(String trans) {
+        this.trans = trans;
+    }
+}
