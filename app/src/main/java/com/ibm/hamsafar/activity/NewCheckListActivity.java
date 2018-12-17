5c9814fc12a443b82c01e1a02dab45d9f4eefec4
rahimi <m.rahimi@behpardaz.net>
2018-12-17 15:06:12 +0330

    listeners were added

--- app/src/main/java/com/ibm/hamsafar/activity/NewCheckListActivity.java
+++ app/src/main/java/com/ibm/hamsafar/activity/NewCheckListActivity.java
@@ -1,4 +1,34 @@
 package com.ibm.hamsafar.activity;
 
-public class NewCheckListActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.ibm.hamsafar.R;
+
+public class NewCheckListActivity extends Activity {
+
+    private Button toolbarBack = null;
+    private TextView toolbarTitle = null;
+    private RecyclerView
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_check_list);
+
+        toolbarBack = findViewById(R.id.toolbar_back);
+        toolbarTitle = findViewById(R.id.toolbar_text);
+
+        toolbarBack.setOnClickListener(view -> onBackPressed());
+
+        toolbarTitle.setText( getResources().getString(R.string.check_list_title));
+
+
+
+    }
+
 }
