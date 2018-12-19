dba30048608c5ab08a0fc35ee53b52aa3ddeaab0
rahimi <m.rahimi@behpardaz.net>
2018-12-19 14:45:49 +0330

    added methods used for items menu

--- app/src/main/java/com/ibm/hamsafar/activity/NewCheckListActivity.java
+++ app/src/main/java/com/ibm/hamsafar/activity/NewCheckListActivity.java
@@ -2,6 +2,7 @@ package com.ibm.hamsafar.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
@@ -10,9 +11,6 @@ import android.widget.TextView;
 import com.ibm.hamsafar.R;
 import com.ibm.hamsafar.adapter.CheckListAdapter;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class NewCheckListActivity extends Activity {
 
     private Button toolbarBack = null;
@@ -20,7 +18,8 @@ public class NewCheckListActivity extends Activity {
     private RecyclerView recyclerView = null;
     private LinearLayoutManager linearLayoutManager = null;
     private CheckListAdapter adapter = null;
-    private List<String> data;
+    private String[] data;
+    private CoordinatorLayout coordinatorLayout = null;
 
 
     @Override
@@ -31,6 +30,7 @@ public class NewCheckListActivity extends Activity {
         toolbarBack = findViewById(R.id.toolbar_back);
         toolbarTitle = findViewById(R.id.toolbar_text);
         recyclerView = findViewById(R.id.cl_recycler_view);
+        coordinatorLayout = findViewById(R.id.coordinator_layout);
 
         toolbarBack.setOnClickListener(view -> onBackPressed());
 
@@ -38,10 +38,13 @@ public class NewCheckListActivity extends Activity {
 
         linearLayoutManager = new LinearLayoutManager(NewCheckListActivity.this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
-        data = Arrays.asList(getResources().getStringArray(R.array.checklist_items));
+        data = getResources().getStringArray(R.array.checklist_items);
         adapter = new CheckListAdapter(this, data);
         recyclerView.setAdapter(adapter);
 
     }
 
+
+    //item edit dialog
+    public void openEditDialog
 }
