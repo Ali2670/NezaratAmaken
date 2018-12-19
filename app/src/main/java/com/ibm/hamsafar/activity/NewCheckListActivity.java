e8938dc0058e8fdf22a176e05cf749e807e4bfc1
rahimi <m.rahimi@behpardaz.net>
2018-12-19 09:28:32 +0330

    string array has benn changed to List if String

--- app/src/main/java/com/ibm/hamsafar/activity/NewCheckListActivity.java
+++ app/src/main/java/com/ibm/hamsafar/activity/NewCheckListActivity.java
@@ -2,17 +2,25 @@ package com.ibm.hamsafar.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.ibm.hamsafar.R;
+import com.ibm.hamsafar.adapter.CheckListAdapter;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class NewCheckListActivity extends Activity {
 
     private Button toolbarBack = null;
     private TextView toolbarTitle = null;
-    private RecyclerView
+    private RecyclerView recyclerView = null;
+    private LinearLayoutManager linearLayoutManager = null;
+    private CheckListAdapter adapter = null;
+    private List<String> data;
 
 
     @Override
@@ -22,12 +30,17 @@ public class NewCheckListActivity extends Activity {
 
         toolbarBack = findViewById(R.id.toolbar_back);
         toolbarTitle = findViewById(R.id.toolbar_text);
+        recyclerView = findViewById(R.id.cl_recycler_view);
 
         toolbarBack.setOnClickListener(view -> onBackPressed());
 
         toolbarTitle.setText( getResources().getString(R.string.check_list_title));
 
-
+        linearLayoutManager = new LinearLayoutManager(NewCheckListActivity.this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        data = Arrays.asList(getResources().getStringArray(R.array.checklist_items));
+        adapter = new CheckListAdapter(this, data);
+        recyclerView.setAdapter(adapter);
 
     }
 
