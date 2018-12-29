7aae7e31abf9efdf16731efe3ca3395d64a5c419
rahimi <m.rahimi@behpardaz.net>
2018-12-29 16:42:45 +0330

    id added

--- app/src/main/java/com/ibm/hamsafar/object/TripInfo.java
+++ app/src/main/java/com/ibm/hamsafar/object/TripInfo.java
@@ -1,15 +1,26 @@
 package com.ibm.hamsafar.object;
 
+import java.io.Serializable;
+
 /**
  * Created by maryam on 12/17/2018.
  */
-public class TripInfo {
+public class TripInfo implements Serializable {
 
+    private Integer id;
     private String port;
     private String des;
     private String start;
     private String end;
     private String trans;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getPort() {
         return port;
