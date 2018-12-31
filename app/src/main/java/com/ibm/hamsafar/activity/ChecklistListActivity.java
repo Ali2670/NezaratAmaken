8b5d119ceb6483054d707b37ef865421bf472190
rahimi <m.rahimi@behpardaz.net>
2018-12-31 08:04:32 +0330

    item on click listener added

--- app/src/main/java/com/ibm/hamsafar/activity/ChecklistListActivity.java
+++ app/src/main/java/com/ibm/hamsafar/activity/ChecklistListActivity.java
@@ -11,8 +11,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.ibm.hamsafar.R;
-import com.ibm.hamsafar.adapter.TripListAdapter;
-import com.ibm.hamsafar.object.TripInfo;
+import com.ibm.hamsafar.adapter.ChecklistAdapter;
+import com.ibm.hamsafar.object.Checklist;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,50 +30,49 @@ public class ChecklistListActivity extends Activity {
     private RecyclerView recyclerView = null;
     private FloatingActionButton add = null;
     private LinearLayoutManager linearLayoutManager = null;
-    private TripListAdapter adapter;
-    private List<TripInfo> listData;
+    private ChecklistAdapter adapter;
+    private List<Checklist> listData;
 
 
     @Override
     protected void attachBaseContext(Context base) {
-        super.attachBaseContext(CalligraphyContextWrapper.wrap( base ));
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(base));
     }
 
 
     @Override
     public void onCreate(Bundle bundle) {
-        super.onCreate( bundle );
-        setContentView( R.layout.trip_list );
+        super.onCreate(bundle);
+        setContentView(R.layout.checklist_list);
 
         toolbarBack = findViewById(R.id.toolbar_back);
         toolbarTitle = findViewById(R.id.toolbar_text);
-        recyclerView = findViewById(R.id.trip_list_recycler_view);
-        add = findViewById(R.id.trip_list_add);
+        recyclerView = findViewById(R.id.cl_list_recycler_view);
+        add = findViewById(R.id.cl_list_add);
 
         toolbarBack.setOnClickListener(view -> onBackPressed());
 
-        toolbarTitle.setText( getResources().getString(R.string.trip_list_title));
+        toolbarTitle.setText(getResources().getString(R.string.cl_list_title));
 
         linearLayoutManager = new LinearLayoutManager(ChecklistListActivity.this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
-        getTripList();
-        adapter = new TripListAdapter(this, listData);
+        getChecklistList();
+        adapter = new ChecklistAdapter(context, listData);
         recyclerView.setAdapter(adapter);
 
-        add.setOnClickListener(view -> startActivity( new Intent( ChecklistListActivity.this, TripActivity.class )));
+        add.setOnClickListener(view -> startActivity(new Intent(ChecklistListActivity.this, CheckListActivity.class)));
 
     }
 
-    private void getTripList() {
+    private void getChecklistList() {
         listData = new ArrayList<>();
-        for(int i=21; i>0; i-- ) {
-            TripInfo tripInfo = new TripInfo();
-            tripInfo.setPort( "port" + i );
-            tripInfo.setDes( "destination" + i );
-            tripInfo.setStart( "1397/09/" + i );
-            tripInfo.setEnd( "1397/10/" + i );
-            tripInfo.setTrans( "trans" + i );
-            listData.add( tripInfo );
+        for (int i = 21; i > 0; i--) {
+            Checklist checklist = new Checklist();
+            checklist.setId(i);
+            checklist.setItem_one("1st item " + i);
+            checklist.setItem_two("2nd item " + i);
+            checklist.setItem_three("3rd item " + i);
+            listData.add(checklist);
         }
     }
 
