381925f7a353f213585b045537548c41acfeda9a
rahimi <m.rahimi@behpardaz.net>
2018-12-31 11:27:58 +0330

    run services

--- app/src/main/java/com/ibm/hamsafar/activity/TripActivity.java
+++ app/src/main/java/com/ibm/hamsafar/activity/TripActivity.java
@@ -3,9 +3,12 @@ package com.ibm.hamsafar.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AlertDialog;
@@ -25,13 +28,21 @@ import android.widget.Toast;
 import com.hamsafar.persianmaterialdatetimepicker.date.DatePickerDialog;
 import com.hamsafar.persianmaterialdatetimepicker.utils.PersianCalendar;
 import com.ibm.hamsafar.R;
+import com.ibm.hamsafar.asyncTask.ListHttp;
+import com.ibm.hamsafar.asyncTask.TaskCallBack;
 import com.ibm.hamsafar.object.TripInfo;
 import com.ibm.hamsafar.utils.DateUtil;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 
+import hamsafar.ws.model.JsonCodec;
+import hamsafar.ws.request.SubmitTripRequest;
+import hamsafar.ws.response.SubmitTripResponse;
+import hamsafar.ws.util.service.ServiceNames;
+import ibm.ws.WsResult;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class TripActivity extends Activity implements DatePickerDialog.OnDateSetListener {
@@ -84,7 +95,6 @@ public class TripActivity extends Activity implements DatePickerDialog.OnDateSet
         parentLayout = findViewById(R.id.trip_parent);
 
 
-
         toolbarTitle.setText(getResources().getString(R.string.trip_title));
 
         toolbarBack.setOnClickListener(view -> onBackPressed());
@@ -106,23 +116,21 @@ public class TripActivity extends Activity implements DatePickerDialog.OnDateSet
         initialise();
 
         portText.setOnClickListener(view -> {
-            if ( checkInternetConnection() ) {
+            if (checkInternetConnection()) {
                 String title = getResources().getString(R.string.trip_port_title);
                 String[] items = getResources().getStringArray(R.array.province_array);
                 showSearchListDialog(portText, title, items);
-            }
-            else {
+            } else {
                 Toast.makeText(context, getResources().getString(R.string.internet_error), Toast.LENGTH_SHORT).show();
             }
         });
 
         desText.setOnClickListener(view -> {
-            if ( checkInternetConnection() ) {
+            if (checkInternetConnection()) {
                 String title = getResources().getString(R.string.trip_des_title);
                 String[] items = getResources().getStringArray(R.array.country_array);
                 showSearchListDialog(desText, title, items);
-            }
-            else {
+            } else {
                 Toast.makeText(context, getResources().getString(R.string.internet_error), Toast.LENGTH_SHORT).show();
             }
         });
@@ -141,13 +149,12 @@ public class TripActivity extends Activity implements DatePickerDialog.OnDateSet
         transText.setOnClickListener(view -> {
             String[] array = getResources().getStringArray(R.array.trip_transpose);
             String title = getResources().getString(R.string.trip_trans_title);
-            showListDialog( transText, title, array );
+            showListDialog(transText, title, array);
         });
 
 
-
         save.setOnClickListener(view -> {
-            saveAction( view );
+            saveAction(view);
         });
 
 
@@ -160,22 +167,22 @@ public class TripActivity extends Activity implements DatePickerDialog.OnDateSet
         //set now for start date
         startDateText.setText(DateUtil.getCurrentDate());
 
-        transText.setText( getResources().getString(R.string.trip_default_trans) );
+        transText.setText(getResources().getString(R.string.trip_default_trans));
 
-        portText.setText("");
+        desText.setText("");
         endDateText.setText("");
     }
 
     //save button action
-    private void saveAction( View view ) {
+    private void saveAction(View view) {
         boolean hasError = false;
 
-        if( desText.getText().toString().trim().equals("")) {
+        if (desText.getText().toString().trim().equals("")) {
             desLayout.setError(getResources().getString(R.string.Exc_800001));
             hasError = true;
         }
 
-        if( endDateText.getText().toString().trim().equals("") ) {
+        if (endDateText.getText().toString().trim().equals("")) {
             endDateLayout.setError(getResources().getString(R.string.Exc_800002));
             hasError = true;
         }
@@ -188,12 +195,12 @@ public class TripActivity extends Activity implements DatePickerDialog.OnDateSet
                 setTripInfo();
                 insertTripIntoDB();
                 Intent intent;
-                if( checkList.isChecked() ) {
+                if (checkList.isChecked()) {
                     intent = new Intent(TripActivity.this, CheckListActivity.class);
                     intent.putExtra("trip_info", tripInfo);
                 } else {
-                    intent = new Intent( TripActivity.this, Main.class );
-                    Toast.makeText( context, getResources().getString(R.string.trip_save_message), Toast.LENGTH_SHORT).show();
+                    intent = new Intent(TripActivity.this, Main.class);
+                    Toast.makeText(context, getResources().getString(R.string.trip_save_message), Toast.LENGTH_SHORT).show();
                 }
                 startActivity(intent);
                 finish();
@@ -205,17 +212,17 @@ public class TripActivity extends Activity implements DatePickerDialog.OnDateSet
 
 
     /*
-    * show list dialog
-    *
-    * */
-    private void showListDialog(final EditText editText, String title, String[] items ) {
+     * show list dialog
+     *
+     * */
+    private void showListDialog(final EditText editText, String title, String[] items) {
         final AlertDialog.Builder builder = new AlertDialog.Builder(TripActivity.this);
         builder.setCancelable(true);
         LayoutInflater inflater = getLayoutInflater();
         View listViewDialog = inflater.inflate(R.layout.alertdialog_with_list, null);
         builder.setView(listViewDialog);
         TextView listTitle = listViewDialog.findViewById(R.id.listAlertDialogTitle);
-        listTitle.setText( title );
+        listTitle.setText(title);
         ListView listView = listViewDialog.findViewById(R.id.dialogList);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.simple_expandable_list_item, items);
         listView.setAdapter(adapter);
@@ -223,29 +230,51 @@ public class TripActivity extends Activity implements DatePickerDialog.OnDateSet
         dialog.show();
         listView.setOnItemClickListener((parent, view, position, id) -> {
             String value = ((TextView) view).getText().toString();
-            editText.setText( value );
+            editText.setText(value);
             dialog.dismiss();
         });
     }
 
 
     private void setTripInfo() {
-        tripInfo.setPort( portText.getText().toString().trim() );
-        tripInfo.setDes( desText.getText().toString().trim() );
-        tripInfo.setStart( startDateText.getText().toString().trim() );
-        tripInfo.setEnd( endDateText.getText().toString().trim() );
-        tripInfo.setTrans( transText.getText().toString().trim() );
+        tripInfo.setPort(portText.getText().toString().trim());
+        tripInfo.setDes(desText.getText().toString().trim());
+        tripInfo.setStart(startDateText.getText().toString().trim());
+        tripInfo.setEnd(endDateText.getText().toString().trim());
+        tripInfo.setTrans(transText.getText().toString().trim());
     }
 
 
     /*
-    * insert trip info into DB
-    * get its id
-    * pass trip info to checklist activity if needed( contains trip id )
-    * */
+     * insert trip info into DB
+     * get its id
+     * pass trip info to checklist activity if needed( contains trip id )
+     * */
 
     private void insertTripIntoDB() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences( this );
+        Integer user_id = sharedPreferences.getInt("user_id", 0 );
 
+        SubmitTripRequest request = new SubmitTripRequest();
+        request.setUserId( user_id );
+        request.setTitle(portText.getText().toString() + " " + startDateText.getText().toString());
+        request.setSourceId(0);
+        request.setDestinationId(1);
+        request.setStartDate( startDateText.getText().toString().trim() );
+        request.setStartTime( DateUtil.getCurrentTime() );
+        request.setEndDate( endDateText.getText().toString().trim() );
+        request.setEndTime( DateUtil.getCurrentTime() );
+        request.setTripType((byte) 0);
+        request.setTripStatus((byte) 0);
+
+        TaskCallBack<Object> submitTripResponse = result -> {
+            SubmitTripResponse ress = JsonCodec.toObject((Map) result, SubmitTripResponse.class);
+
+            Toast.makeText(context, ress.getTripId().toString(), Toast.LENGTH_SHORT).show();
+
+        };
+        AsyncTask<Object, Void, WsResult> list = new ListHttp(submitTripResponse, this, null, ServiceNames.SUBMIT_TRIP, false);
+        list.execute(request);
     }
 
 
@@ -285,7 +314,6 @@ public class TripActivity extends Activity implements DatePickerDialog.OnDateSet
     }
 
 
-
     private boolean checkInternetConnection() {
         //if connected return true
         ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -313,10 +341,10 @@ public class TripActivity extends Activity implements DatePickerDialog.OnDateSet
     public void onDateSet(com.hamsafar.persianmaterialdatetimepicker.date.DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
         int moth = monthOfYear + 1;
         //set date
-        if( DATE_PICKER_CALLER.equals("start")) {
+        if (DATE_PICKER_CALLER.equals("start")) {
             startDateText.setText(year + "/" + moth + "/" + dayOfMonth);
         }
-        if( DATE_PICKER_CALLER.equals("end")) {
+        if (DATE_PICKER_CALLER.equals("end")) {
             endDateText.setText(year + "/" + moth + "/" + dayOfMonth);
         }
 
@@ -325,7 +353,7 @@ public class TripActivity extends Activity implements DatePickerDialog.OnDateSet
         String end = endDateText.getText().toString();
         Date startDate = null;
         Date endDate = null;
-        if( !start.equals("") && !end.equals("") ) {
+        if (!start.equals("") && !end.equals("")) {
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
             try {
                 startDate = simpleDateFormat.parse(start);
@@ -333,17 +361,15 @@ public class TripActivity extends Activity implements DatePickerDialog.OnDateSet
             } catch (ParseException e) {
                 e.printStackTrace();
             }
-            if( startDate.after(endDate) ) {
-                if( DATE_PICKER_CALLER.equals("start") ) {
+            if (startDate.after(endDate)) {
+                if (DATE_PICKER_CALLER.equals("start")) {
                     startDateLayout.setError(getResources().getString(R.string.trip_start_after_end_exc));
                     endDateLayout.setError(null);
-                }
-                else {
+                } else {
                     endDateLayout.setError(getResources().getString(R.string.trip_end_before_start_exc));
                     startDateLayout.setError(null);
                 }
-            }
-            else {
+            } else {
                 startDateLayout.setError(null);
                 endDateLayout.setError(null);
             }
@@ -352,7 +378,7 @@ public class TripActivity extends Activity implements DatePickerDialog.OnDateSet
 
 
     //show list with search view as dialog
-    private void showSearchListDialog( EditText editText, String title, String[] items ) {
+    private void showSearchListDialog(EditText editText, String title, String[] items) {
         final AlertDialog.Builder builder = new AlertDialog.Builder(TripActivity.this);
         builder.setCancelable(true);
         LayoutInflater inflater = getLayoutInflater();
@@ -362,8 +388,9 @@ public class TripActivity extends Activity implements DatePickerDialog.OnDateSet
         Button close = listViewDialog.findViewById(R.id.dialog_toolbar_close);
         ListView listView = listViewDialog.findViewById(R.id.listWithSearchList);
         EditText searchInput = listViewDialog.findViewById(R.id.listWithSearchInput);
-        listTitle.setText( title );
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.simple_expandable_list_item, items);        listView.setAdapter(adapter);
+        listTitle.setText(title);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.simple_expandable_list_item, items);
+        listView.setAdapter(adapter);
         //adding search functionality to ListView using inputSearch
         searchInput.addTextChangedListener(new TextWatcher() {
             @Override
@@ -385,7 +412,7 @@ public class TripActivity extends Activity implements DatePickerDialog.OnDateSet
         dialog.show();
         listView.setOnItemClickListener((parent, view, position, id) -> {
             String value = ((TextView) view).getText().toString();
-            editText.setText( value );
+            editText.setText(value);
             dialog.dismiss();
         });
         close.setOnClickListener(new View.OnClickListener() {
@@ -397,14 +424,13 @@ public class TripActivity extends Activity implements DatePickerDialog.OnDateSet
     }
 
 
-
     @Override
     public void onBackPressed() {
         android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(TripActivity.this);
         builder.setMessage(getResources().getString(R.string.exit_message));
         builder.setPositiveButton(getResources().getString(R.string.exit_save_changes),
                 (dialogInterface, i) -> {
-                    saveAction( parentLayout );
+                    saveAction(parentLayout);
                 });
         builder.setNegativeButton(getResources().getString(R.string.exit_cancel),
                 (dialogInterface, i) -> {
@@ -412,8 +438,7 @@ public class TripActivity extends Activity implements DatePickerDialog.OnDateSet
                 });
         builder.setNeutralButton(getResources().getString(R.string.exit_discard),
                 (dialogInterface, i) -> {
-                    initialise();
-                    dialogInterface.cancel();
+                    finish();
                 });
         builder.show();
     }
