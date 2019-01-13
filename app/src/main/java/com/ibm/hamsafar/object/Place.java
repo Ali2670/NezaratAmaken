2b3652922ef2e4083badf6a28a52e5e3d1656ae6
rahimi <m.rahimi@behpardaz.net>
2019-01-13 09:14:27 +0330

    place POJO class

---
+++ app/src/main/java/com/ibm/hamsafar/object/Place.java
@@ -0,0 +1,55 @@
+package com.ibm.hamsafar.object;
+
+import java.io.Serializable;
+
+/**
+ * Created by maryam on 1/13/2019.
+ */
+public class Place implements Serializable {
+
+    private Integer id;
+    private String name;
+    private String type;
+    private String address;
+    private String phone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
