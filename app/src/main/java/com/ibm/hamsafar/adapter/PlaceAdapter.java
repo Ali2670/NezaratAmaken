368522ff6a854c299ad325f5ceac12fd521210be
rahimi <m.rahimi@behpardaz.net>
2019-01-14 14:34:36 +0330

    adapter used for handling place

--- app/src/main/java/com/ibm/hamsafar/adapter/PlaceAdapter.java
+++ app/src/main/java/com/ibm/hamsafar/adapter/PlaceAdapter.java
@@ -6,9 +6,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.ibm.hamsafar.R;
+import com.ibm.hamsafar.object.Place;
+
+import java.util.List;
 
 /**
  * Created by Wizard on 12/11/2017.
@@ -17,18 +20,18 @@ import com.ibm.hamsafar.R;
 public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.ViewHolder> {
 
     private Context context;
-    private int[] posters;
+    private List<Place> places;
 
-    public PlaceAdapter(Context context, int[] posters) {
+    public PlaceAdapter(Context context, List<Place> places) {
         this.context = context;
-        this.posters = posters;
+        this.places = places;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card,parent,false);
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.place_card,parent,false);
 
         return new ViewHolder(itemView);
     }
@@ -36,24 +39,30 @@ public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.ViewHolder> 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
 
-        holder.image.setImageResource(posters[position]);
+        holder.name.setText( places.get(position).getName() );
+        holder.phone.setText( places.get(position).getPhone() );
+        holder.address.setText( places.get(position).getAddress() );
+
     }
 
 
 
     @Override
     public int getItemCount() {
-
-        return posters.length;
+        return places.size();
     }
 
     public  class ViewHolder extends  RecyclerView.ViewHolder implements View.OnClickListener{
 
-        public ImageView image;
+        public TextView name;
+        public TextView phone;
+        public TextView address;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            image = itemView.findViewById(R.id.card_image);
+            name = itemView.findViewById(R.id.place_card_name);
+            phone = itemView.findViewById(R.id.place_card_phone);
+            address = itemView.findViewById(R.id.place_card_address);
             itemView.setOnClickListener( this );
         }
 
